91a6ce804f10170dd1f999fe71078cc803fb7f22
rmc8 <kmprmc@gmail.com>
2022-06-29 18:09:24 +0900

    fix

--- rsexec/__main__.py
+++ rsexec/__main__.py
@@ -2,12 +2,25 @@ import os
 import sys
 
 
+def exit_if(exit_sw: bool, text: str):
+    if exit_sw:
+        print(text)
+        exit()
+
+
 def main():
     cur_path: str = os.getcwd()
     os.system(f"cd {cur_path}")
+    exit_if(
+        len(sys.argv) == 1,
+        "No arguments were found that contain the path to the .rs script",
+    )
     rust_source: str = sys.argv[1]
-    os.system(f"rustc {rust_source}")
-    os.system(f"./{rust_source[:-3]}")
+    exit_if(rust_source[-3:] != ".rs", "No .rs extension")
+    cmd_lines : list = [f"rustc {rust_source}", f"./{rust_source[:-3]}", ]
+    for cmd in cmd_lines:
+        print(cmd)
+        os.system(cmd)
 
 
 if __name__ == "__main__":
